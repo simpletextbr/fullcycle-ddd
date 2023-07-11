97bfe89bc75dda09d82e8d8855242701afab20a4
Wesley Praca <simpletextbr@gmail.com>
2023-07-11 16:08:53 -0300

    Aplicando regras simples para validancao

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,19 @@
+// UMA ENTIDATE POR PADRÃO DEVE SE AUTO-VALIDAR GARANTINDO SUA CONSISTÊNCIA 
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
-    _active: boolean = true;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (!this._name || this._name.length === 0) throw new Error("Name is required")
+        if (!this._id || this._id.length === 0) throw new Error("ID is required")
     }
     
     get id(): string {
@@ -22,15 +28,15 @@ class Customer {
         return this._address;
     }
 
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
-    }
-    
-    set address(address: string) {
-        this._address = address;
+        this.validate();
     }
 
     activate(){
+        if(!this._address || this._address.length === 0) 
+            throw new Error("Address is mandatory to activate a customer")
+
         this._active = true;
     }
 
