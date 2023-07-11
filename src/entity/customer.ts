dd091641f9a33973699b453234ccfc860eef98e3
Wesley Praca <simpletextbr@gmail.com>
2023-07-11 15:51:33 -0300

    Imputando regras especificas em entidades

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,13 +2,14 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = true;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
     }
-
+    
     get id(): string {
         return this._id;
     }
@@ -24,8 +25,17 @@ class Customer {
     set name(name: string) {
         this._name = name;
     }
-
+    
     set address(address: string) {
         this._address = address;
     }
+
+    activate(){
+        this._active = true;
+    }
+
+    deactivate(){
+        this._active = false;
+    }
 }
+
