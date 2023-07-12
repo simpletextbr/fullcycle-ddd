c763ee50e8712958b30083d615602d05431dd062
Wesley Praca <simpletextbr@gmail.com>
2023-07-11 21:33:26 -0300

    Setando agregados part 2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Address from "./entity/VOs/address";
 import OrderItem from "./entity/VOs/orderItem";
 import Customer from "./entity/customer";
+import Order from "./entity/order";
 
 let customer = new Customer("1", "John Doe");
 const address = new Address("Main St", 123, "12345", "NY");
@@ -11,3 +12,4 @@ customer.activate();
 const item1 = new OrderItem("1", "item 1", 1, 10);
 const item2 = new OrderItem("1", "item 2", 1, 20);
 const item3 = new OrderItem("1", "item 3", 1, 30);
+const order = new Order("1", customer._id, [item1, item2, item3]);
