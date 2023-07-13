152f6820839af97c056896c5a5452249729b7992
Wesley Praca <simpletextbr@gmail.com>
2023-07-12 22:03:15 -0300

    Ajustes no package json

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const address = new Address("Main St", 123, "12345", "NY");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "item 1", 1, 10);
-const item2 = new OrderItem("1", "item 2", 1, 20);
-const item3 = new OrderItem("1", "item 3", 1, 30);
-const order = new Order("1", customer._id, [item1, item2, item3]);
+const item1 = new OrderItem("1", "item 1", 1, 10, "123");
+const item2 = new OrderItem("1", "item 2", 1, 20, "123");
+const item3 = new OrderItem("1", "item 3", 1, 30, "123");
+const order = new Order("1", customer.id, [item1, item2, item3]);
