5dbe46d7095cfec276845bbeabbad4cc1d57d9e6
Wesley Praca <simpletextbr@gmail.com>
2023-07-17 16:47:13 -0300

    adicionando inclusion no sonar-project

--- src/__tests__/entity/customer.spec.ts
+++ src/__tests__/entity/customer.spec.ts
@@ -3,31 +3,31 @@ import Customer from "../../entity/customer";
 
 describe("Customer unit tests", () => {
 
-    it("should throw error when id is empty", () => {
+    test("should throw error when id is empty", () => {
         expect(() => new Customer("", "John Doe")).toThrowError("ID is required");
     });
 
-    it("should throw error when name is empty", () => {
+    test("should throw error when name is empty", () => {
         expect(() => new Customer("123", "")).toThrowError("Name is required");
     });
 
-    it("should be able to change name", () => {
+    test("should be able to change name", () => {
         const customer = new Customer("123", "John")
         customer.changeName("John Doe")
         expect(customer.name ).toBe("John Doe");
     });
 
-    it("should be able to change name", () => {
+    test("should be able to change name", () => {
         const customer = new Customer("123", "John")
         expect(() => { customer.changeName("") }).toThrowError("Name is required");
     });
 
-    it("should return user id", () => {
+    test("should return user id", () => {
         const customer = new Customer("123", "John")
         expect(customer.id).toBe("123");
     });
 
-    it("should be able to activate an user", () => {
+    test("should be able to activate an user", () => {
         const customer = new Customer("123", "John");
         const address = new Address("Rua 1", 123, "12345-000", "BH");
         customer.Address = address;
@@ -37,7 +37,7 @@ describe("Customer unit tests", () => {
         expect(customer.isActive()).toBe(true);
     });
 
-    it("should be able to activate an user", () => {
+    test("should be able to activate an user", () => {
         const customer = new Customer("123", "John");
         const address = new Address("Rua 1", 123, "12345-000", "BH");
         customer.Address = address;
@@ -47,12 +47,12 @@ describe("Customer unit tests", () => {
         expect(customer.isActive()).toBe(true);
     });
 
-    it("should be not able to activate an user", () => {
+    test("should be not able to activate an user", () => {
         const customer = new Customer("123", "John")
         expect(() => { customer.activate() }).toThrowError("Address is mandatory to activate a customer");
     });
 
-    it("should be able to deactivate an user", () => {
+    test("should be able to deactivate an user", () => {
         const customer = new Customer("123", "John")
         customer.deactivate()
         expect(customer.isActive()).toBe(false);
