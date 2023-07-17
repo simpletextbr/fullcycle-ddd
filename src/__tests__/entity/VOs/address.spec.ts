7998e01124c99b5e045a0cc3d7204884de46197e
Wesley Praca <simpletextbr@gmail.com>
2023-07-17 15:58:13 -0300

    Ajuste em testes na chamada de construtores

--- src/__tests__/entity/VOs/address.spec.ts
+++ src/__tests__/entity/VOs/address.spec.ts
@@ -7,19 +7,19 @@ describe("Address unit tests", () => {
     })
 
     it("should throw error when street is empty", () => {
-        expect(() => { new Address("", 123, "12345-000", "BH") }).toThrowError("Street is required");
+        expect(() => new Address("", 123, "12345-000", "BH")).toThrowError("Street is required");
     });
 
     it("should throw error when number is empty", () => {
-        expect(() => { new Address("Rua 1", NaN, "12345-000", "BH") }).toThrowError("Number is required");
+        expect(() => new Address("Rua 1", NaN, "12345-000", "BH")).toThrowError("Number is required");
     })
 
     it("should throw error when zip is empty", () => {
-        expect(() => { new Address("Rua 1", 123, "", "BH") }).toThrowError("Zip Code is required");
+        expect(() => new Address("Rua 1", 123, "", "BH")).toThrowError("Zip Code is required");
     })
 
     it("should throw error when city is empty", () => {
-        expect(() => { new Address("Rua 1", 123, "12345-000", "") }).toThrowError("City is required");
+        expect(() => new Address("Rua 1", 123, "12345-000", "")).toThrowError("City is required");
     })
 
 });
